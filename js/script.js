f23e8701ab0b41d9b120a04b5dbb2f23c14096a1
Mari <mari-jen@hotmail.com>
2022-06-02 15:07:55 -0300

    posição scroll animação

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 import ScrollSuave from "./modules/scroll-suave.js";
-import initScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-nav.js";
 import initModal from "./modules/modal.js";
@@ -9,13 +8,13 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import initScroll from "./modules/scroll-animacao.js";
 
 
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
-initScroll();
 initAccordion();
 initTabNav();
 initModal();
@@ -25,4 +24,5 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
+initScroll();
 
