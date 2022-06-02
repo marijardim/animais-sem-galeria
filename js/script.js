07eabb84b982cb5d32520034516b43f53338f4f1
Mari <mari-jen@hotmail.com>
2022-06-02 17:23:55 -0300

    modal refatorado com classe

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import ScrollSuave from "./modules/scroll-suave.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tab-nav.js";
-import initModal from "./modules/modal.js";
+import Modal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
@@ -18,9 +18,9 @@ const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabNav.init();
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]')
 
 initTabNav();
-initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
